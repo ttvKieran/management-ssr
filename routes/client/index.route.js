4c3419b86288ad8b85ff54cf7290b44ebc2a5619
ttvKieran <tatruongvuptit@gmail.com>
2025-03-17 22:25:51 +0700

    menu drop down category for client

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const productRoutes = require('./product.route');
 const homeRoutes = require('./home.route');
+const categoryMiddleware = require('../../middlewares/client/category.middleware');
 
 module.exports = (app) => {
+    app.use(categoryMiddleware.category);
     app.use('/', homeRoutes);
     app.use('/products', productRoutes);
 }
